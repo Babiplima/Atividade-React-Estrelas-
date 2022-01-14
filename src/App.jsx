05a5125daf8e3b6e7e574bc4874d760147fc334a
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-14 18:25:56 -0300

    Segundo Commit Cards Prontos

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import './App.css'
-import React from 'react'
-import Primeiro from './componentes/Primeiro'
-import ComParametro from './componentes/ComParametro'
-import Card from './componentes/Layout/Card'
+import './App.css';
+import React from 'react';
 
-export default props => (
+import Primeiro from './componentes/Primeiro';
+import ComParametro from './componentes/ComParametro'
+import ComFilhos from './componentes/ComFilhos'
+import Card from './componentes/Layout/Card';
+import Repeticao from "./componentes/Repeticao";
+import Condicional from "./componentes/Condicional";
+import CondicionalComIf from "./componentes/CondicionalComIf";
+
+
+
+export default (props) => (
+    
     <div className="App">
-     <Card titulo="Componente com Filhos">
-        <ComFilhos>
+    <Card titulo="#06 - Condicional v2">
+    <CondicionalComIf numero={11}></CondicionalComIf>       
+    </Card>
+     <Card titulo="#05 - Condicional v1">
+     <Condicional numero={10}></Condicional>       
+    </Card>
+    <Card titulo="#04 - Repetição">
+     <Repeticao></Repeticao>       
+    </Card>
+    <Card titulo="#03 - Componente Com Filhos">
+     <ComFilhos>
         <ul>
             <li>Ana</li>
             <li>Bia</li>
             <li>Carlos</li>
             <li>Daniel</li>
         </ul>
-    </ComFilhos>
+        </ComFilhos>
     </Card>
 
-    <Card titulo="Componente com Parâmetro">
-    <ComParametro titulo="Esse é o titulo"
-    subtitulo="Esse é o subtítulo" />
-    <Card titulo="Componente com Parâmetro">
-    <ComParametro titulo="Esse é o titulo"
+    <Card titulo="#02 - Componente Com Parametro">
+     <ComParametro titulo="Esse é o titulo"
     subtitulo="Esse é o subtítulo" />
     </Card>
-    <Card titulo="Primeiro Componente">
-    <Primeiro/>
-    </Card>
-    {/*
-{/*
-
-<ComParametro titulo="Opa" subtitulo="Epa"/>*/}
-
+     <Card titulo="#01 - Primeiro Componente">
+         <Primeiro/>
+         </Card>
+  
 </div>
 );
